6d98081454e90c4f6b364b28289b89c3e1a22c1f
Eric Traub <eric@hedgeable.com>
2017-09-08 17:56:37 -0400

    introduce & use promises with actions & reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'aaf206b949986c9f3f6a59d5644ead3d';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
